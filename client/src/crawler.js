c097c99c72bf25c818e34554ea28ab03661259d8
kimhyun5u <gustndlfrl@gmail.com>
2022-09-21 19:26:40 +0900

    체크리스트 로컬 스토리지에 저장하도록 수정

--- client/src/crawler.js
+++ client/src/crawler.js
@@ -21,8 +21,6 @@
                         reader.onloadend = function(e) {
                             myFile = JSON.parse(this.result);
                             
-                            let checkList = document.getElementById("checkList");
-                            
                             updateCheckList();
                         };
                         reader.readAsText(file);
@@ -41,10 +39,58 @@
         });
     }
 
-    function updateCheckList() {
+    // update checklist
+    async function updateCheckList() {
+        let checkList = document.getElementById("checkList");
+
         checkList.tBodies[0].innerHTML = ``;
+        
         for(const section of myFile[selectedBelt]) {
-            checkList.tBodies[0].innerHTML += `<tr><td><input type="checkbox"></td><td>${section.name}</td></tr>`;
+            const savedData = await getLocal(section.name);
+            const isChecked = !!savedData[section.name];
+            if(isChecked) {
+                checkList.tBodies[0].innerHTML += `<tr><td><input class="check-box" type="checkbox" checked></td><td>${section.name}</td></tr>`;
+            } else {
+                checkList.tBodies[0].innerHTML += `<tr><td><input class="check-box" type="checkbox"></td><td>${section.name}</td></tr>`;
+            }    
+        }
+
+        updateCheckBoxEventListener();
+    }
+
+    function getLocal(key) {
+        return new Promise((resovle, reject) => {
+            chrome.storage.local.get(key, (item) => {
+                if(chrome.runtime.lastError) {
+                    return reject(chrome.runtime.lastError);
+                }
+
+                resovle(item);
+            });
+        });
+    }
+    function updateCheckBoxEventListener() {
+        
+        let checkBoxes = document.getElementsByClassName("check-box");
+    
+        for(let checkBox of checkBoxes) {
+            checkBox.addEventListener('change', () => {
+                if(checkBox.checked) {
+                    const key = checkBox.parentElement.parentElement.children[1].textContent;
+                    const keyValue = {};
+                    keyValue[key] = true;
+                    // save local storage
+                    chrome.storage.local.set(keyValue, ()=> {
+                        console.log(keyValue);
+                    });
+                } else {
+                    const key = checkBox.parentElement.parentElement.children[1].textContent;
+                    const keyValue = {};
+                    keyValue[key] = false;
+                    // save local storage
+                    chrome.storage.local.set(keyValue);
+                }
+            });
         }
     }
 })();
